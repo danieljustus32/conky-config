0e126185dc669fdaf9df2c194c98125f7c16d4e7
Daniel <daniel.justus32@gmail.com>
2021-03-03 21:23:58 -0600

    Add 3rd disk, still need to read data on disks dynamically depending on what's connected

--- disks.sh
+++ disks.sh
@@ -24,3 +24,14 @@ Label:                   \${color1}$DEVICE\$color
 Amount Used:
   ${USED} \${color $COLOR}\${fs_bar 6,320 ${MOUNT}} \$color\${alignr}${SIZE}"
 done
+df -hT /dev/sdb1 | grep -v '/boot' | head -4 | sed 's/vfat/FAT32/g;s/fuseblk/NTFS/g;' | grep /dev | while read DEVICE TYPE SIZE USED FREE PERCENT MOUNT && [ $i -le 6 ]
+do
+  label=3
+  i=$((i+1))
+  USED=$(printf "%4s" $USED | sed 's/B/ B  /;s/\([KMGT]\)/ \1iB/')
+  SIZE=$(printf "%4s" $SIZE | sed 's/B/ B  /;s/\([KMGT]\)/ \1iB/')
+  echo "\${color1}$label:\${color} Mount Point :   \$color1$MOUNT \$color\${alignr} Filesystem:   \${color1}$TYPE\$color
+Label:                   \${color1}$DEVICE\$color 
+Amount Used:
+  ${USED} \${color $COLOR}\${fs_bar 6,320 ${MOUNT}} \$color\${alignr}${SIZE}"
+done
